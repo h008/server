0a9395ba80f3c583053756eea4b5811b849c2d89
h_008 <h_008@mac.com>
2021-10-16 12:37:01 +0900

    カレンダーの六曜表示

---
+++ @h008/qreki/qreki.d.ts
@@ -0,0 +1,9 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {number} y
+* @param {number} m
+* @param {number} d
+* @returns {any}
+*/
+export function from_ymd(y: number, m: number, d: number): any;
---
+++ @h008/qreki/qreki.js
@@ -0,0 +1,5 @@
+import * as wasm from "/var/www/html/@h008/qreki/qreki_bg.wasm";
+import * as js from "/var/www/html/@h008/qreki/qreki_bg.js";
+export default {
+    js,wasm
+}
